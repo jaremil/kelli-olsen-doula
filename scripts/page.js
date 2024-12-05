3603b07ae610530653f4bf9691737015c7abd1aa
Jade Miller <mil19017@byui.edu>
2024-12-04 19:19:47 -0700

    buttons started but not finished

---
+++ scripts/page.js
@@ -0,0 +1,21 @@
+// navigation scroll
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(link => {
+      link.addEventListener('click', event => {
+        event.preventDefault();
+        
+        const targetId = link.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+          });
+        }
+      });
+    });
+  });
